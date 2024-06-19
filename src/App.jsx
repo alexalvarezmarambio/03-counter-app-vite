83231769a5dc93e40eaf748e13bc9d7dd9a0bb1a
Alex Alvarez <alexalvarezmarambio@gmail.com>
2024-06-19 01:27:07 -0400

    feat(FirstApp): proptypes and defaultprops

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import { FirstApp } from "./FirstApp";
 
 export const App = () => {
-  return (
-    <>
-      <h1>Hello World App</h1>
-      <FirstApp title="Hola, soy Alex" subTitle={"Desarrollador Frontend"} />
-    </>
-  );
+  return <FirstApp title={"Hola, soy Alex"} />;
 };
--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,19 @@
-const getNombre = () => "Alex Alvarez Marambio";
+import PropTypes from "prop-types";
 
-export const FirstApp = ({ title = getNombre(), subTitle }) => {
+export const FirstApp = ({ title, subTitle }) => {
   return (
     <>
       <h1>{title}</h1>
       <p>{subTitle}</p>
     </>
   );
+};
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string,
+};
+
+FirstApp.defaultProps = {
+  subTitle: "No hay Subtitulo",
 };
